6ed67910cc6e9c91b06b7ae5367bdb7926daf699
kiranshidruk <kiranshidruk27@gmail.com>
2024-08-14 23:39:00 -0400

    folder structure added using script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MLOps-Prod-US-VISA-Prediction
+
+
+- Flowchart: https://whimsical.com/
+- MLOPs Tool: https://www.evidentlyai.com/
+- MongoDB: https://account.mongodb.com/account/login
+- Data link: https://www.kaggle.com/datasets/moro23/easyvisa-dataset
+
---
+++ template.py
@@ -0,0 +1,50 @@
+import os
+from pathlib import Path
+
+project_name = "us_visa"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",  
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipline/__init__.py",
+    f"{project_name}/pipline/training_pipeline.py",
+    f"{project_name}/pipline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+        "app.py",
+    "requirements.txt",
+    "Dockerfile",
+    ".dockerignore",
+    "demo.py",
+    "setup.py",
+    "config/model.yaml",
+    "config/schema.yaml"
+]
+
+
+
+for filePath in list_of_files:
+    filePath = Path(filePath)
+
+    print(filePath)
+    filedir, filename = os.path.split(filePath)
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+    if (not os.path.exists(filePath)) or (os.path.getsize(filePath)==0):
+        with open(filePath, "w") as file:
+            pass
+    else:
+        print(f"file is already present at: {filePath}")
